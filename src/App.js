78e1ff03ac68cd04cd2aa06fd6ed847a1b4e27d8
sarnickaa <sarnicka.aleks@gmail.com>
2018-08-18 16:41:13 -0400

    add logout option

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App container">
             <nav>
               <Link to="/" className="links" id="home-link">HOME</Link>
+              <Link to="/" className="links nav">logout</Link>
               <Link to="/sign-up" className="links nav">register</Link>
               <Link to="/sign-in" className="links nav">login</Link>
             </nav>
