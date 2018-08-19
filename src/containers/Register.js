0eab88ce666e041ba446d1eef0fda8ddea9d1741
sarnickaa <sarnicka.aleks@gmail.com>
2018-08-18 22:03:29 -0400

    allow user registration
    added logic to allow a user to register
    added logic to re-route to login view on success

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./Register.css";
 
 export default class Register extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+  }
+
+  onChange = (e) => {
+    const state = this.state
+    state[e.target.name] = e.target.value
+    this.setState(state)
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    const { email, password, password_confirmation } = this.state
+
+    axios.post('http://localhost:4741/sign-up', { email, password, password_confirmation })
+      .then((result) => {
+        console.log(result)
+        // how do i redirect to /login on success here?
+        this.props.history.push('/sign-in')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+
   render() {
+    const { email, password, password_confirmation } = this.state
     return (
     <div>
-      <form className="Register-form">
+      <form className="Register-form" onSubmit={this.onSubmit}>
         <label>Sign Up for GLUG!</label>
         <br />
-          <input type="email" placeholder="your email"></input>
+          <input type="email" placeholder="your email" name="email" value={email} onChange={this.onChange}></input>
           <br />
-          <input type="password" placeholder="password"></input>
+          <input type="password" placeholder="password" name="password" value={password} onChange={this.onChange}></input>
           <br />
-          <input type="password" placeholder="confirm password"></input>
+          <input type="password" placeholder="confirm password" name="password_confirmation" value={password_confirmation} onChange={this.onChange}></input>
           <br />
             <button type="submit">Register</button>
         </form>
