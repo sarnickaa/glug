3d930e6925fa6d819ce3e85dff558792604b98c2
sarnickaa <sarnicka.aleks@gmail.com>
2018-08-20 08:46:24 -0400

    attempting to fix refactoring

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import "./Login.css";
+import Main from './Main.js'
+import { Route, Switch } from "react-router-dom";
 
 export default class Login extends Component {
 
@@ -9,7 +11,8 @@ export default class Login extends Component {
     super(props)
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      token: ''
     }
   }
 
@@ -34,11 +37,16 @@ export default class Login extends Component {
     axios.post('http://localhost:4741/sign-in', { email, password })
       .then((result) => {
         console.log(result)
-        this.props.setLoginInfo(result.data.user.token)
+        // this.props.setLoginInfo(result.data.user.token)
+        this.setState({
+          token: result.data.user.token
+        })
+        console.log(this.state.token)
         // localStorage.setItem('token', result.data.user.token)
         console.log(result.data.user.token)
         // console.log(localStorage.getItem('token'))
-        this.props.history.push('/main')
+        // this.props.history.push('/main')
+        // <Main />
       })
       .catch((error) => {
         console.error(error)
@@ -59,6 +67,8 @@ export default class Login extends Component {
           <br />
             <button type="submit">Log In</button>
         </form>
+        {this.state && this.state.token &&
+        <Main />}
       </div>
     );
   }
