e37d9c75b3e20872bde0022b3db5d3153616d935
sarnickaa <sarnicka.aleks@gmail.com>
2018-08-29 15:07:48 -0400

    allow users to upload an image and view it

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Home from "./containers/Home";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
 import Main from "./containers/Main";
+import Gallery from "./containers/Gallery";
 
 export default (props) => {
   // global props coming through are sent from App.js
@@ -18,6 +19,7 @@ export default (props) => {
 {/* render method used to pass props to a route component. routeProps are different from the global props coming through to the <Routes /> component. routeProps must be specified if things like history and location need to be accessed
 history and location must be passed in this case as the token generated through login will be attached to the history.push() object and pushed through to the /main route on the success of an axios call */}
       <Route path="/main" render={(routeProps) => <Main setViewLinkState={props.setViewLinkState} viewLinkState={props.viewLinkState} {...routeProps}/>} />
+      <Route path="/gallery" exact component={Gallery} />
     </Switch>
   )
 }
---
+++ src/containers/Gallery.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import { apiUrl } from '../server.js'
+import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
+// import { Modal,ModalManager,Effect} from 'react-dynamic-modal'
+// import Card from '@material-ui/core/Card';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+
+
+import "./Gallery.css";
+// import MyModal from "./MyModal.js"
+
+export default class Gallery extends Component {
+
+  constructor(props) {
+    // console.log(props)
+    super(props)
+    this.state = {
+      gallery: []
+    }
+  }
+
+
+  componentDidMount() {
+     // Request for images tagged xmas
+     axios.get('https://res.cloudinary.com/dcegqfaze/image/list/test.json')
+         .then(res => {
+             console.log(res.data.resources);
+             this.setState({gallery: res.data.resources});
+         });
+ }
+
+  uploadWidget() {
+    let _this = this
+// https://www.sitepoint.com/bind-javascripts-this-keyword-react/
+
+    // https://stackoverflow.com/questions/47287916/cloudinary-widget-use-in-react
+      window.cloudinary.openUploadWidget({ cloud_name: 'dcegqfaze', upload_preset: 'esunn7gq', tags:['test']},
+          function(error, result) {
+              console.log(result);
+              _this.setState({gallery: _this.state.gallery.concat(result)})
+          });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Galleria</h1>
+        <div className="gallery">
+          <CloudinaryContext cloudName="dcegqfaze">
+              {this.state.gallery.map(data => {
+                return (
+                  <div className="responsive" key={data.public_id}>
+                    <div className="img">
+                      <a target="_blank" href={`https://res.cloudinary.com/dcegqfaze/image/upload/${data.public_id}.jpg`}>
+                        <Image publicId={data.public_id}>
+                          <Transformation
+                          crop="fit"
+                          width="300"
+                          height="300"
+                          dpr="auto"
+                          responsive_placeholder="blank" />
+                        </Image>
+                      </a>
+                    <div className="desc">Created at {data.created_at}</div>
+                    </div>
+                  </div>
+                        )
+                          })
+                            }
+            </CloudinaryContext>
+          <div>
+            <button onClick={this.uploadWidget.bind(this)}>Add Image</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// export default WineForm
